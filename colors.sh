8c5efe8b1e7830045d975d622008eb5d851cf7d9
wick3dr0se <wick3dr0se@protonmail.com>
2022-05-20 16:39:59 -0500

    output identifiers

--- colors.sh
+++ colors.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+_out(){
+	printf "\033[1;40;97m%s\033[0m\n" "$1"
+}
 
 4bit() {
 	echo -ne "\033[1;${1}${i}m${1}${i}\033[0m"
 }
 
-echo '4-bit ANSI escape codes: \033[1;??m..\033[0m'
+
+_out '4-bit ANSI escape sequence'
+_out 'Usage: \033[1;??m .. \033[0m'
 for i in {0..7} ; do
 	4bit 3 # FG code 
 	4bit 4 # BG code
@@ -17,7 +22,9 @@ done
 	echo -ne "\033[${1}8;5;${i}m${i}\033[0m"
 }
 
-printf '\n%s\n' '8-bit ANSI escape codes: \033[?8;5;???m..\033[0m' 
+echo
+_out '8-bit ANSI escape sequence'
+_out 'Usage: \033[?8;5;???m .. \033[0m'
 for i in {0..256} ; do
 	8bit 3 #FG code
 	8bit 4 #BG code
